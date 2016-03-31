74f65dd0f82a7eae36dec8c1ae0c42611d146fcc
Liu, Yang <yang@aglobalway.com>
2016-03-31 07:48:13 -0700

    export as a formatted .doc resume

---
+++ theme/views-data-export-doc-header.tpl.php
@@ -0,0 +1,39 @@
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  </head>
+  <style type="text/css">
+	#responsive-resume .header {
+	    font-weight: bold;
+	    border-top-style: solid;
+	    border-top-width: 1px;
+	    border-bottom-style: solid;
+	    border-bottom-width: 1px;
+	    margin-top: 14px;
+	}
+	.row {
+	    margin-left: -15px;
+	    margin-right: -15px;
+	}
+	.row:before, .row:after{
+		content: " ";
+	    display: table;
+	}
+	.col-sm-2, .col-sm-10{
+		position: relative;
+	    min-height: 1px;
+	    padding-left: 15px;
+	    padding-right: 15px;
+	    float: left;
+	}
+
+  </style>
+
+  <body>
+    <table>
+    <?php 
+    	if (0 == 1){
+	    	print $header_row; 
+    	}
+    ?>
+    <tbody>
